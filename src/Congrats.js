66169a370bac4ea405f4b832a959e96803839b4a
Gdraven99 <DavidJYopp@gmail.com>
2020-01-19 16:36:41 -0500

    added congrats component tests

---
+++ src/Congrats.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+/**
+* Functional react component for Congrats message
+* @ function
+* @ returns {JSX.Element} Rendered Component or null if success prop is false
+*/
+
+export default () => {
+  return (
+    <div></div>
+  )
+}
