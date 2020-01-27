d50942c8dd794727ca428606652849a43d3aa51b
Gdraven99 <DavidJYopp@gmail.com>
2020-01-26 19:49:31 -0500

    Installed redux-thunk middleware

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,5 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
 
@@ -11,7 +11,8 @@ import rootReducer from '../src/reducers';
 * @returns {Store} - redux store
 */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
